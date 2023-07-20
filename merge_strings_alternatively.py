568f3a0eee89fa1f7f006d799d34f0db6404e0a3
kherin <kherin.j.s.bundhoo@gmail.com>
2023-07-20 17:56:40 +0000

    WIP: Completed first testcase

--- merge_strings_alternatively.py
+++ merge_strings_alternatively.py
@@ -1,19 +1,21 @@
+import itertools
+
 class MergeStringsAlternatively:
   def merge_strings_alternatively(self, word1, word2):
     list_one = list(word1)
     list_two = list(word2)
-  
-    print(f'list_one: {list_one}')
-    print(f'list_two: {list_two}')
 
-    zipped_list = tuple(zip(*list_one, *list_two))
+    zipped_list = list(zip(list_one, list_two))
     print(f'zipped_list: {zipped_list}')
+    flattened_list = list(itertools.chain(*zipped_list))
+    print(f'flattened_list: {flattened_list}')
+    return " ".join(flattened_list)
     
   
   def main(self):
-    self.merge_strings_alternatively('abc', 'pqr')
-    # assert merge_strings_alternatively('abc', 'pqr') == 'a p b q c r'
-    # assert merge_strings_alternatively('ab', 'pqrs') == 'a p b q   r   s'
-    # assert merge_strings_alternatively('abcd', 'pq') == 'a p b q c   d'
+    # print(self.merge_strings_alternatively('abc', 'pqr') == 'a p b q c r')
+    # print(self.merge_strings_alternatively('ab', 'pqrs') == 'a p b q   r   s')
+    print(self.merge_strings_alternatively('ab', 'pqrs'))
+    # print(self.merge_strings_alternatively('abcd', 'pq') == 'a p b q c   d')
 
   
