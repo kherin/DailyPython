93eebc21643c737519ee3f83bfbfc531be2c420f
kherin <kherin.j.s.bundhoo@gmail.com>
2023-07-22 15:05:10 +0000

    Solved: merge string alternatively

--- merge_strings_alternatively.py
+++ merge_strings_alternatively.py
@@ -5,28 +5,26 @@ class MergeStringsAlternatively:
     list_one = list(word1)
     list_two = list(word2)
 
-    zipped_list = list(zip(list_one, list_two))
-    print(f'zipped_list: {zipped_list}')
-    
-    flattened_list = list(itertools.chain(*zipped_list))
-    print(f'flattened_list: {flattened_list}')
-    
-    remainder = None
-    starting_point = abs(len(list_one) - len(list_two))
-    
-    if len(list_one) > len(list_two):
-      remainder = list_one[starting_point:]
-    elif len(list_one) < len(list_two):
-      remainder = list_two[starting_point:]
+    max_length = max(len(list_one), len(list_two))
+    result = []
 
-    print(f'remainder: {remainder}')
-    
-    return " ".join(flattened_list)
+    for index in range(max_length):
+      if index > len(list_one) - 1:
+        result.append(' ')
+      else:
+        result.append(list_one[index])
+
+      if index > len(list_two) - 1:
+        result.append(' ')
+      else:
+        result.append(list_two[index])
+
+    return ' '.join(result).strip()
     
   
   def main(self):
-    # print(self.merge_strings_alternatively('abc', 'pqr') == 'a p b q c r')
-    # print(self.merge_strings_alternatively('ab', 'pqrs') == 'a p b q   r   s')
+    print(self.merge_strings_alternatively('abc', 'pqr') == 'a p b q c r')
+    print(self.merge_strings_alternatively('ab', 'pqrs') == 'a p b q   r   s')
     print(self.merge_strings_alternatively('abcd', 'pq') == 'a p b q c   d')
 
   
