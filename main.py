414b85ae10704eecc4a316945891a86a8ce506a9
kherin <kherin.j.s.bundhoo@gmail.com>
2023-08-14 17:00:53 +0000

    WIP: playing with strings

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from merge_strings_alternatively import MergeStringsAlternatively
 from filename_extraction import extract
+from strings import play
 
 
 def main():
-    extract()
+    play()
 if __name__ == '__main__':
     main()
---
+++ strings.py
@@ -0,0 +1,4 @@
+def play(): 
+  classifier_image = ' TEST '
+  classifier_image.strip().lower()
+  print(f'classifier image is {classifier_image.strip().lower()}')
