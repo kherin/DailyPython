1012c7a46f2119358ea1b2368916114490212462
Kherin <kherin.j.s.bundhoo@gmail.com>
2023-08-17 22:54:46 +0400

    WIP: created django channels project

---
+++ channels_app/channels_app/routing.py
@@ -0,0 +1,9 @@
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import path
+from channels_app.consumers import Consumer
+
+application = ProtocolTypeRouter({
+    'websocket': URLRouter([
+        path('ws/chat/', Consumer.as_asgi()),
+    ]),
+})
